bd327d676cfa00fe226ba1fb66b95aba09327887
Master_PK <pk.keawampai@gmail.com>
2015-12-21 16:39:22 +0700

    ออกแบบ GUI New1

--- app/src/main/java/thailand/keawampai/pakpoom/mytraffic/MainActivity.java
+++ app/src/main/java/thailand/keawampai/pakpoom/mytraffic/MainActivity.java
@@ -2,13 +2,30 @@ package thailand.keawampai.pakpoom.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bindwidget();
+
     }
+
+    private void bindwidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutButton = (Button) findViewById((R.id.button));
+    }
+
 }
